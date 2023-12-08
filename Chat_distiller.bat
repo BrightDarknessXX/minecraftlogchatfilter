06f8d6d810ea66e18a6f7e4a450547de2c13a114
BrightDarknessXX <56561628+BrightDarknessXX@users.noreply.github.com>
2023-12-08 16:57:16 +0100

    Add files via upload

---
+++ Chat_distiller.bat
@@ -0,0 +1,34 @@
+@echo off
+if not exist "chat_distiller\TMP" (md chat_distiller\TMP)
+
+setlocal enabledelayedexpansion
+
+FOR %%x in ("*.log") DO (
+    set "filename=%%x"
+    set "chd=chat_distiller"
+    set "chdTMP=!chd!\TMP"
+    set "chdTMPlogfile=!chdTMP!\!filename!"
+    set "newname=!filename:.log=_chat-distiller.log!"
+
+    set "chkfile=!chd!\!newname!"
+    if exist "!chkfile!" (
+        Echo !newname! already exists.
+    ) else (
+        type %%x | find /i "Chat" >> !chdTMPlogfile!
+
+        set "rendir=!chdTMP!\!filename!"
+        ren !rendir! !newname!
+        echo !newname! distilled.
+    )
+)
+
+cd !chdTMP!
+if exist *.log (
+    move *.log ..\
+)
+cd ..
+rd TMP /S /Q
+
+endlocal
+
+pause
